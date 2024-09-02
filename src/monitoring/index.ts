20b0b27ee363abff5b8201b1f033353ac753b387
Kian Banke Larsen <kilar20@student.sdu.dk>
2024-09-02 19:03:18 +0200

    Added dashboards to grafana

--- src/monitoring/index.ts
+++ src/monitoring/index.ts
@@ -70,6 +70,74 @@ new k8s.helm.v3.Chart("grafana", {
                 ]
             }
         },
+        // https://github.com/dotdc/grafana-dashboards-kubernetes
+        dashboardProviders: {
+            "dashboardproviders.yaml": {
+                apiVersion: 1,
+                providers: [
+                    {
+                        name: 'grafana-dashboards-kubernetes',
+                        orgId: 1,
+                        folder: 'Kubernetes',
+                        type: "file",
+                        disableDeletion: true,
+                        editable: true,
+                        options: {
+                            path: "/var/lib/grafana/dashboards/grafana-dashboards-kubernetes"
+                        }
+                    },
+                    {
+                        name: 'grafana-dashboards-node',
+                        orgId: 1,
+                        folder: 'Node',
+                        type: "file",
+                        disableDeletion: true,
+                        editable: true,
+                        options: {
+                            path: "/var/lib/grafana/dashboards/grafana-dashboards-node"
+                        }
+                    }
+                ]
+            }
+        },
+        dashboards: {
+            "grafana-dashboards-kubernetes": {
+                "k8s-system-api-server": {
+                    url: "https://raw.githubusercontent.com/dotdc/grafana-dashboards-kubernetes/master/dashboards/k8s-system-api-server.json",
+                    token: ''
+                },
+                "k8s-system-coredns": {
+                    url: "https://raw.githubusercontent.com/dotdc/grafana-dashboards-kubernetes/master/dashboards/k8s-system-coredns.json",
+                    token: ''
+                },
+                "k8s-views-global": {
+                    url: "https://raw.githubusercontent.com/dotdc/grafana-dashboards-kubernetes/master/dashboards/k8s-views-global.json",
+                    token: ''
+                },
+                "k8s-views-namespaces": {
+                    url: "https://raw.githubusercontent.com/dotdc/grafana-dashboards-kubernetes/master/dashboards/k8s-views-namespaces.json",
+                    token: ''
+                },
+                "k8s-views-nodes": {
+                    url: "https://raw.githubusercontent.com/dotdc/grafana-dashboards-kubernetes/master/dashboards/k8s-views-nodes.json",
+                    token: ''
+                },
+                "k8s-views-pods": {
+                    url: "https://raw.githubusercontent.com/dotdc/grafana-dashboards-kubernetes/master/dashboards/k8s-views-pods.json",
+                    token: ''
+                },
+                // "container-advisor": {
+                //     url: "https://grafana.com/api/dashboards/14282/revisions/1",
+                //     token: ''
+                // }
+            },
+            "grafana-dashboards-node": {
+                "node-exporter": {
+                    url: "https://raw.githubusercontent.com/rfmoz/grafana-dashboards/master/prometheus/node-exporter-full.json",
+                    token: ''
+                }
+            }
+        },
         ingress: {
             enabled: true,
             ingressClassName: "nginx",
