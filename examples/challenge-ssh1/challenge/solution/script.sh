64caf5918c14220c4bfd32fa0d2d9ee9c84c305d
Kian Banke Larsen <kilar20@student.sdu.dk>
2025-03-27 19:14:07 +0100

    Modified test (currently not working on VM), align with challenges and updated notebook

---
+++ examples/challenge-ssh1/challenge/solution/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Start SSH tunnel as background service
+sshpass -p 'test' ssh -o StrictHostKeyChecking=accept-new -p 8022 test@$SSH_SERVICE_INTERNAL_URL -L 8080:web1.ctf:80 -N &
+
+# Wait for SSH tunnel to be established
+sleep 5
+
+# Get index.html that serves the flag
+wget localhost:8080
+
+# Extract flag from index.html
+FLAG=$(cat index.html | grep -o flag{.*})
+
+# Print the flag to the solution output
+echo $FLAG >> /run/solution/flag.txt
